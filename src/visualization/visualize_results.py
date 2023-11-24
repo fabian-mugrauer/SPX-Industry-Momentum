feee15ed3d48ff4b32e27bc630d20fe38f29cf12
Marco Bischoff <marcobischoff@Marcos-MacBook-Air.local>
2023-11-24 21:35:28 +0100

    updated visualization class to cope with requirements

--- src/visualization/visualize_results.py
+++ src/visualization/visualize_results.py
@@ -5,18 +5,18 @@ import numpy as np
 
 class Visualizer:
     def __init__(self):
-        # Initialization can be used to set up any necessary properties or default values
-        pass
+        # Set default font size for readability
+        plt.rcParams.update({'font.size': 11})
+        # Set a colorblind-friendly palette as default
+        self.palette = sns.color_palette("colorblind")
+        # Set default line width for clarity
+        self.line_width = 2
 
     def create_colorheatmap(self, weights):
         """
         Creates and displays a color-blind friendly heatmap.
-
-        Parameters:
-        - weights (2D array-like): The data to be visualized in the heatmap.
         """
         plt.figure(figsize=(10, 6))
-        # Using the 'cividis' colormap which is designed to be colorblind-friendly
         sns.heatmap(weights, cmap='cividis', cbar=True)
         plt.title("Heatmap of weights over months")
         plt.xlabel("Assets")
@@ -25,14 +25,7 @@ class Visualizer:
 
     def plot_strategies_with_benchmark(self, dates, strategy_returns_list, benchmark_returns, labels, start_month):
         """
-        Plots strategy returns (single or multiple) along with a benchmark return in a color-blind friendly way.
-
-        Parameters:
-        - dates (array-like): Dates for the x-axis.
-        - strategy_returns_list (array-like or list of array-like): Total returns of one or multiple strategies.
-        - benchmark_returns (array-like): Total returns of the benchmark.
-        - labels (list of str): List of labels for the strategy return series.
-        - start_month (int): The starting index for the plot.
+        Plots strategy returns along with a benchmark return.
         """
         # Check if strategy_returns_list is a single series or a list of series
         if isinstance(strategy_returns_list, np.ndarray):
@@ -42,8 +35,8 @@ class Visualizer:
         if len(strategy_returns_list) > 5:
             raise ValueError("Cannot plot more than 5 strategy return series.")
 
-        # Define a list of color-blind friendly colors for strategies
-        strategy_colors = ['seagreen', 'darkorange', 'purple', 'grey', 'brown']
+        # Use the default palette for strategy colors
+        strategy_colors = self.palette[:len(strategy_returns_list)]
 
         # Create a new figure and axis
         fig, ax = plt.subplots(figsize=(10, 6))
@@ -51,11 +44,11 @@ class Visualizer:
         # Plot each strategy return series
         for i, returns in enumerate(strategy_returns_list):
             cumulative_returns = np.cumprod(1 + returns[start_month:]) - 1
-            ax.plot(dates[start_month:], cumulative_returns, label=labels[i], color=strategy_colors[i], linestyle='-')
+            ax.plot(dates[start_month:], cumulative_returns, label=labels[i], color=strategy_colors[i], linestyle='-', linewidth=self.line_width)
 
         # Plot the benchmark return series
         cumulative_benchmark_returns = np.cumprod(1 + benchmark_returns[start_month:].squeeze()) - 1
-        ax.plot(dates[start_month:], cumulative_benchmark_returns, label='Benchmark', color='darkblue', linestyle='--')
+        ax.plot(dates[start_month:], cumulative_benchmark_returns, label='Benchmark', color='darkblue', linestyle='--', linewidth=self.line_width)
 
         # Add labels, title, and legend
         ax.set_xlabel('Date')
@@ -70,19 +63,12 @@ class Visualizer:
         plt.tight_layout()
         plt.show()
 
-    def plot_robustness_check(self, check_range, sharp_ratios, check_type, line_width=2, color='blue'):
+    def plot_robustness_check(self, check_range, sharp_ratios, check_type):
         """
         Plots the results of a robustness check.
-
-        Parameters:
-        - check_range (range): The range of values used in the robustness check.
-        - sharp_ratios (list or array-like): The calculated Sharp Ratios for each value in the check range.
-        - check_type (str): The type of robustness check ('lookback', 'investment_horizon', 'holdings').
-        - line_width (int, optional): The width of the line in the plot. Default is 2.
-        - color (str, optional): The color of the line in the plot. Default is 'blue'.
         """
         plt.figure()
-        plt.plot(check_range, sharp_ratios, linewidth=line_width, color=color)
+        plt.plot(check_range, sharp_ratios, linewidth=self.line_width, color='blue')
         plt.xlabel(check_type.replace("_", " "))
         plt.ylabel("Sharpe Ratio")
         plt.title(f"Robustness Check: {check_type.replace('_', ' ').title()}")
